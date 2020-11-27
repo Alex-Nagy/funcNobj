ae03e3bde406a05202383af38ba1186e96dfcf97
Alex <Nagalender@users.noreply.github.com>
2020-11-27 01:11:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let john = {
 };
 */
 
+/*
 let Person = function(name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
@@ -31,3 +32,41 @@ mark.calculateAge();
 console.log(john.name + " " + john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+*/
+/*
+// Object.create
+let personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth);
+    }
+};
+
+let john = Object.create(personProto); 
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+let jane = Object.create(personProto,{
+    name: { value: 'Jane' },
+    yearOfBirth: { value: 1969},
+    job: { value: 'designer' },
+});
+ */
+
+ 
+ // Primitives vs objects
+
+let a = 23;
+let b = a;
+a = 46;
+console.log(a); //46
+console.log(b); //23 
+
+let obj1 = {
+    name: 'John',
+    age: 26
+};
+let obj2 = obj1;
+obj1.age = 30;
+console.log (obj1.age); //30
+console.log (obj2.age); //30
